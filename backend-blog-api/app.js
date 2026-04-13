48e90a126b13ace271121df181f6720ab9117f34
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2026-04-13 06:04:43 +0000

    Enable cors from any origin for testing purposes, to allow access from any origin, as I will next start creating the frontend react part of project

--- backend-blog-api/app.js
+++ backend-blog-api/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
+app.use(cors());
 
+app.use(express.json());
 const postRouter = require("./routes/postRouter");
 const authRouter = require("./routes/authRouter");
 const userRouter = require("./routes/userRouter");
