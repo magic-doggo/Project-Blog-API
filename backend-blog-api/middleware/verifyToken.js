c27724c0f499d2e09192e38d14c0af16441e919b
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2026-05-03 08:34:23 +0000

    Update verifyToken to return 401 error instead of 403 403 is used in controllers for authorization. 401 is used by verifyToken for authentication. frontend useSecureFetch will log users out on 401 error

--- backend-blog-api/middleware/verifyToken.js
+++ backend-blog-api/middleware/verifyToken.js
@@ -3,11 +3,14 @@ const jwt = require("jsonwebtoken");
 function verifyToken(req, res, next) {
     const bearerHeader = req.headers['authorization'];
     if (!bearerHeader) {
-        return res.status(403).json({error: "Access denied, no token provided"})
+        return res.status(401).json({ error: "Access denied, no token provided" })
     }
     const token = bearerHeader.split(' ')[1];
     jwt.verify(token, process.env.jwtSecretKey, (err, decodedPayLoad) => {
-        if (err) return res.status(403).json({error: "Invalid or expired token"});
+        if (err) {
+            console.log("jwt verify error, but token is provided:")
+            return res.status(401).json({ error: "Invalid or expired token" });
+        }
         req.user = decodedPayLoad;
         next();
     });
--- backend-blog-api/utils/permissions.js
+++ backend-blog-api/utils/permissions.js
@@ -1,5 +1,5 @@
 function isAuthorized(user, targetCommentOrPost) { //only targetting comments so far, since only admin can make posts, and can edit any post
-    console.log("isauthorized user:", user, "targetcommentorpost: ", targetCommentOrPost)
+    // console.log("isauthorized user:", user, "targetcommentorpost: ", targetCommentOrPost)
     if (user.role === "ADMIN") return true;
     if (user.userId === targetCommentOrPost.authorId) return true;
     return false;
