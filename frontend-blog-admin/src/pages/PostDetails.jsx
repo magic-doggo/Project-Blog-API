9883b035a1e8ad31b3e2faa1ab4e782e786620dd
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2026-05-05 06:34:51 +0000

    Create page where admins can view details of specified post

---
+++ frontend-blog-admin/src/pages/PostDetails.jsx
@@ -0,0 +1,144 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSecureFetch } from "../hooks/useSecureFetch";
+
+
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export default function PostDetails() {
+    const navigate = useNavigate();
+    const { token } = useAuth();
+    const { postId } = useParams();
+    const id = Number(postId);
+    const idIsValid = Number.isFinite(id) && id > 0;
+
+    const [post, setPost] = useState(null);
+    // const [comments, setComments] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const secureFetch = useSecureFetch();
+
+    useEffect(() => {
+        if (!idIsValid) return;
+        let cancelled = false;
+        async function loadPost() {
+            try {
+                const response = await secureFetch(`${API_BASE_URL}/admin/posts/${id}`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    }
+                });
+                if (!response.ok) {
+                    throw new Error("Failed to fetch post");
+                };
+                const data = await response.json();
+                if (cancelled) return;
+                setPost(data.post ?? null);
+                console.log("data: ", data)
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        loadPost();
+        return () => {
+            cancelled = true;  // stops before setPost if page about to be changed or component loads again
+        };
+    }, [secureFetch, id, idIsValid]);
+
+    if (!idIsValid) {
+        return <div>Please input a valid id</div>;
+    }
+    if (!post) return <p>Loading…</p>;
+
+
+    async function togglePostPublishStatus(targetId, currentlyPublished) {
+        if (!token) return;
+        setErrorMessage("");
+        let valueToChangePublishedTo = !currentlyPublished;
+        try {
+            const response = await secureFetch(`${API_BASE_URL}/admin/posts/${targetId}/status`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ isPublished: valueToChangePublishedTo }),
+            });
+
+            if (!response.ok) {
+                let errorMessage = "Failed to update post publish status";
+                try {
+                    const data = await response.json();
+                    errorMessage = data.error ?? errorMessage;
+                } catch {
+                    console.error("Could not read error message")
+                }
+                setErrorMessage(errorMessage);
+                return;
+            }
+
+            const updatedData = await response.json();
+            // setPost({ ...post, isPublished: updatedData.isPublished, publishedDate: updatedData.publishedDate })
+            setPost((prev) => ({...prev, isPublished: updatedData.isPublished, publishedDate: updatedData.publishedDate}))
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    async function deletePost(targetId) {
+        if (!token) return;
+        setErrorMessage("");
+        try {
+            const response = await secureFetch(`${API_BASE_URL}/admin/posts/${targetId}`, {
+                method: "DELETE",
+            });
+
+            if (!response.ok) {
+                let errorMessage = "Failed to Delete Post";
+                try {
+                    const data = await response.json();
+                    errorMessage = data.error ?? errorMessage;
+                } catch {
+                    console.error("Could not read error message");
+                    console.log(response);
+                }
+                setErrorMessage(errorMessage);
+                return;
+            }
+            navigate("/admin", { replace: true });
+            // setPost(null);
+        } catch (err) {
+            console.error(err);
+            setErrorMessage("Could not reach the server")
+        }
+    }
+
+
+    return (
+        <div>
+            <h2>Post Details:</h2>
+            {errorMessage && <p role="alert">{errorMessage}</p>}
+            <h3>Title: {post.title}</h3>
+            <p>Post Body:</p>
+            <div>{post.body}</div>
+            <p>Author: {post.author?.username ?? "deleted user"}</p>
+            <p>{post.isPublished ? "Published" : "Draft (not published)"} </p>
+            <button type="button" onClick={() => togglePostPublishStatus(post.id, post.isPublished)}>{post.isPublished ? "Unpublish post" : "Publish post"}</button>
+            <button type="button" onClick={() => deletePost(post.id)}>Delete post</button>
+
+
+            {/* <p>NrOfCommentsIconPlaceholder: {post._count.comments}</p> */}
+            {/* <p>Comments:</p>
+            <ul>
+                {comments.map((comment) => {
+                    <li key={comment.id}>
+                        <div>{comment.body}</div>
+                        <div>{comment.author} - {comment.updatedAt ?? comment.publishedDate}</div>
+                    </li>
+                })}
+            </ul> */}
+        </div>
+    )
+}
