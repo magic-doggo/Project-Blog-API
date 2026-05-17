e8bc599cf49f452ef9ed7704002582c1c016a282
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2026-05-17 08:23:29 +0000

    Create navigation bar for both logged in and not logged in users

--- frontend-blog-reader/src/App.jsx
+++ frontend-blog-reader/src/App.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
 // import styles from "./styles/App.module.css"
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <div className="reader-layout">
-      {/* <Navbar/> */}
+      <Navbar/>
 
       <main>
         <Outlet/>
